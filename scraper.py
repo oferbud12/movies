8fca00550340405367deffa7c62c017ad83b4e42
Peleg Keren <pelegker@gmail.com>
2019-05-04 00:06:52 +0300

    fixed scraper_load_data_to_json with better logic

--- scraper.py
+++ scraper.py
@@ -1,56 +1,71 @@
 from selenium import webdriver
 import datetime
+import time
 import json
 from bs4 import BeautifulSoup
 
 
 class RavHenHerzlyiaScraper:
 
+    WEBSITE_NAME = "Rav_Hen_Herzlyia"
+
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.load_website_data_to_json("Rav_Hen_Herzlyia")
-        self.driver.close()
+        self.load_website_data_to_json()
 
     @staticmethod
     def get_current_movies_screens_url():
+
         url = "https://www.rav-hen.co.il/#/buy-tickets-by-cinema?in-cinema=1061&at=%s&view-mode=list" % datetime.date.today()
         return url
 
-    @staticmethod
-    def parse_element(element, element_type):
-        if element_type == "movie_name":
-            movie_name = str(element).split('name">')[1].split('</')[0]
-            return movie_name
-        elif element_type == "movie_screen":
-            element = str(element).replace("amp;", "").split('data-url="')[1].split('" href="#">')
-            url = element[0]
-            presentation_code = url.split("presentationCode=")[1].split("-")[0]
-            screen_time = element[1][:-4]
-            return presentation_code, screen_time, url
+    def load_website_data_to_json(self):
 
-    def load_website_data_to_json(self, website_name):
         self.driver.get(RavHenHerzlyiaScraper.get_current_movies_screens_url())
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
-        movies_names = [RavHenHerzlyiaScraper.parse_element(movie, "movie_name") for movie in soup.find_all(attrs={"class": "qb-movie-name"})]
-        movies_data = [RavHenHerzlyiaScraper.parse_element(movie, "movie_screen") for movie in soup.find_all(attrs={"class": "btn btn-sm btn-primary"})]
         movies_dict = {}
-        for movie_name in movies_names:
-            movies_dict[movie_name] = {"code": movies_data[0][0]}
-            for movie_data in movies_data:
-                if movie_data[0] == movies_dict[movie_name]["code"]:
-                    movies_dict[movie_name][movie_data[1]] = movie_data[2]
-                else:
-                    break
-            movies_data = [movie_data for movie_data in movies_data if movie_data[2] not in movies_dict[movie_name].values()]
+        movies_blocks = soup.find_all("div", attrs={"class": ["row movie-row", "row movie-row first-movie-row"]})
+        for movie_block in movies_blocks:
+            movie_name = str(movie_block).split('name">')[1].split('</')[0]
+            movies_dict[movie_name] = {}
+            screens_data = str(movie_block).replace("amp;", "").split('data-url="')[1:]
+            for screen in screens_data:
+                data = screen.split('" href="#">')
+                screen_url = data[0]
+                screen_time = data[1][:5]
+                movies_dict[movie_name][screen_time] = screen_url
 
         with open("bank.json", "r") as json_file:
             data = json.load(json_file)
-            data[website_name] = movies_dict
+            data[RavHenHerzlyiaScraper.WEBSITE_NAME] = movies_dict
         with open("bank.json", "w") as outfile:
             json.dump(data, outfile)
 
+    @classmethod
+    def go_to_screen(cls, movie_name, screen_time, tickets):
+
+        active_driver = webdriver.Chrome()
+        with open("bank.json", "r") as json_file:
+            data = json.load(json_file)
+
+        try:
+            active_driver.get(data[RavHenHerzlyiaScraper.WEBSITE_NAME][movie_name][screen_time])
+        except KeyError:
+            raise SystemError("I don't know this screen: '%s' at '%s'" % (movie_name, screen_time))
+
+        active_driver.find_element_by_xpath('//select[@class="ddlTicketQuantity"]/option[@value="%s"]' % str(tickets)).click()
+        active_driver.find_element_by_xpath('//a[@id="lbSelectSeats"]').click()
+        active_driver.find_element_by_xpath('//a[@class="u1st_accBtn u1st_accBtnText"]').click()
+        time.sleep(15)
+        active_driver.find_element_by_xpath('//span[@class="slider round"]').click()
+        active_driver.find_element_by_xpath('//app-button[@id="applyBtn"]/button').click()
+
 
 now = RavHenHerzlyiaScraper()
+RavHenHerzlyiaScraper.go_to_screen("הנוקמים: סוף המשחק", "21:30", 3)
+
+
+
 
 
 
